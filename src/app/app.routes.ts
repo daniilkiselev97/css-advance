365b4c2e657a0e262d1f149ed6250236e476ae63
Daniil <dvkiselev@fil-it.ru>
2024-09-12 17:59:44 +0300

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { PositionComponent } from './position/position.component';
+import { ContextComponent } from './context/context.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'position',
+		// component: PositionComponent
+		loadComponent: () => import('./position/position.component').then((c) => c.PositionComponent)
+	},
+
+	{
+		path: 'context',
+		// component: ContextComponent
+		loadComponent: () => import('./context/context.component').then((c) => c.ContextComponent)
+	}
+];
